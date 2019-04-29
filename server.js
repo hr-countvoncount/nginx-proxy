6ed5a888a3ad04211424a17c2d7bb16ae4ecd380
Michael Roeslein <michaeljroeslein@gmail.com>
2019-04-29 16:25:38 -0400

    Updated loaderio filepath

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "./public/")));
+app.get("/loaderio-00660a6577182fa90bf2cc75351199b9", (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "./public/loaderio-00660a6577182fa90bf2cc75351199b9.txt"
+    )
+  );
+});
 
 app.listen(3002, () => console.log("connected on 3002"));
